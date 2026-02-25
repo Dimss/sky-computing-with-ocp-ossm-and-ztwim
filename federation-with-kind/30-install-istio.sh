9414ca57960dc0df02292748aefbdeaef8528d31
Dmitry Kartsev <dimssss@gmail.com>
2026-02-25 12:48:45 +0200

    federation with ocp works

--- federation-with-kind/30-install-istio.sh
+++ federation-with-kind/30-install-istio.sh
@@ -2,11 +2,11 @@ source "$(dirname "$0")/01-define-exports.sh"
 
 for kubeconfig in "${CLUSTER_A_KUBECONFIG}" "${CLUSTER_B_KUBECONFIG}"; do
 
-  helm upgrade --install sail-operator \
-    sail-operator/sail-operator \
-    -n istio-system \
-    --create-namespace \
-    --kubeconfig="${kubeconfig}"
+helm upgrade --install sail-operator \
+  sail-operator/sail-operator \
+  -n istio-system \
+  --create-namespace \
+  --kubeconfig="${kubeconfig}"
 
   kubectl create namespace istio-cni --kubeconfig="${kubeconfig}"
   kubectl label namespace default istio-injection=enabled --kubeconfig="${kubeconfig}"
@@ -19,89 +19,64 @@ metadata:
 spec:
   namespace: istio-cni
 EOF
+
+if [[ "${kubeconfig}" == *"cluster-a"* ]]; then
+  export ISTIO_TRUST_DOMAIN_NAME=$CLUSTER_A
+  export ISTIO_MESH_ID=$CLUSTER_A
+  export ISTIO_MULTI_CLUSTER_NAME=$CLUSTER_A
+  export ISTIO_MULTI_CLUSTER_NETWORK=$NETWORK_A
+else
+  export ISTIO_TRUST_DOMAIN_NAME=$CLUSTER_B
+  export ISTIO_MESH_ID=$CLUSTER_B
+  export ISTIO_MULTI_CLUSTER_NAME=$CLUSTER_B
+  export ISTIO_MULTI_CLUSTER_NETWORK=$NETWORK_B
+fi
+
+cat <<EOF | kubectl apply --kubeconfig="${kubeconfig}" -f -
+apiVersion: sailoperator.io/v1
+kind: Istio
+metadata:
+  name: default
+spec:
+  namespace: istio-system
+  updateStrategy:
+    type: InPlace
+  values:
+    meshConfig:
+      trustDomain: $ISTIO_TRUST_DOMAIN_NAME
+    global:
+      meshID: $ISTIO_MESH_ID
+      multiCluster:
+        clusterName: $ISTIO_MULTI_CLUSTER_NAME
+      network: $ISTIO_MULTI_CLUSTER_NETWORK
+    sidecarInjectorWebhook:
+      templates:
+        spire: |
+          spec:
+            initContainers:
+            - name: istio-proxy
+              volumeMounts:
+              - name: workload-socket
+                mountPath: /run/secrets/workload-spiffe-uds
+                readOnly: true
+            volumes:
+              - name: workload-socket
+                csi:
+                  driver: "csi.spiffe.io"
+                  readOnly: true
+        spireGw: |
+          spec:
+            containers:
+            - name: istio-proxy
+              volumeMounts:
+              - name: workload-socket
+                mountPath: /run/secrets/workload-spiffe-uds
+                readOnly: true
+            volumes:
+              - name: workload-socket
+                csi:
+                  driver: "csi.spiffe.io"
+                  readOnly: true
+EOF
+
 done
-
-cat <<EOF | kubectl apply --kubeconfig="${CLUSTER_A_KUBECONFIG}" -f -
-apiVersion: sailoperator.io/v1
-kind: Istio
-metadata:
-  name: default
-spec:
-  namespace: istio-system
-  updateStrategy:
-    type: InPlace
-  values:
-    meshConfig:
-      trustDomain: $CLUSTER_A
-    sidecarInjectorWebhook:
-      templates:
-        spire: |
-          spec:
-            initContainers:
-            - name: istio-proxy
-              volumeMounts:
-              - name: workload-socket
-                mountPath: /run/secrets/workload-spiffe-uds
-                readOnly: true
-            volumes:
-              - name: workload-socket
-                csi:
-                  driver: "csi.spiffe.io"
-                  readOnly: true
-        spireGw: |
-          spec:
-            containers:
-            - name: istio-proxy
-              volumeMounts:
-              - name: workload-socket
-                mountPath: /run/secrets/workload-spiffe-uds
-                readOnly: true
-            volumes:
-              - name: workload-socket
-                csi:
-                  driver: "csi.spiffe.io"
-                  readOnly: true
-EOF
-
-cat <<EOF | kubectl apply --kubeconfig="${CLUSTER_B_KUBECONFIG}" -f -
-apiVersion: sailoperator.io/v1
-kind: Istio
-metadata:
-  name: default
-spec:
-  namespace: istio-system
-  updateStrategy:
-    type: InPlace
-  values:
-    meshConfig:
-      trustDomain: $CLUSTER_B
-    sidecarInjectorWebhook:
-      templates:
-        spire: |
-          spec:
-            initContainers:
-            - name: istio-proxy
-              volumeMounts:
-              - name: workload-socket
-                mountPath: /run/secrets/workload-spiffe-uds
-                readOnly: true
-            volumes:
-              - name: workload-socket
-                csi:
-                  driver: "csi.spiffe.io"
-                  readOnly: true
-        spireGw: |
-          spec:
-            containers:
-            - name: istio-proxy
-              volumeMounts:
-              - name: workload-socket
-                mountPath: /run/secrets/workload-spiffe-uds
-                readOnly: true
-            volumes:
-              - name: workload-socket
-                csi:
-                  driver: "csi.spiffe.io"
-                  readOnly: true
-EOF
-
--- federation-with-kind/50-install-istio-gateway.sh
+++ federation-with-kind/50-install-istio-gateway.sh
@@ -1,14 +1,18 @@
 source "$(dirname "$0")/01-define-exports.sh"
 
+# regular ingress gateway
 for kubeconfig in "${CLUSTER_A_KUBECONFIG}" "${CLUSTER_B_KUBECONFIG}"; do
   helm upgrade --install istio-gateway -n istio-system \
     istio/gateway \
     --set-json 'podAnnotations={"inject.istio.io/templates":"gateway,spireGw"}' \
-    --set-json 'service.ports=[{"name": "status-port","port": 15021,"protocol": "TCP","targetPort": 15021},{"name": "http2","nodePort": 30980,"port": 80,"protocol": "TCP","targetPort": 80},{"name": "https","nodePort": 30943,"port": 443,"protocol": "TCP","targetPort": 443}]' \
     --kubeconfig="${kubeconfig}"
+#    --set-json 'service.ports=[{"name": "status-port","port": 15021,"protocol": "TCP","targetPort": 15021},{"name": "http2","nodePort": 30980,"port": 80,"protocol": "TCP","targetPort": 80},{"name": "https","nodePort": 30943,"port": 443,"protocol": "TCP","targetPort": 443}]' \
+
 done
 
 
+
+#helm template istio-eastwestgateway istio/gateway -n istio-system --set name=istio-eastwestgateway --set networkGateway=network2
 
 
 
